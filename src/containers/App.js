7eba6328321c7c707cec2057f0044b419b7997a5
Michael Nguyen <nguyenmichaelm@yahoo.com>
2022-02-22 06:12:46 -0800

    Update App.js to use function Component vs class, useState and useEffect to achieve same functionality as previous class Component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,50 +1,55 @@
 //main file that app "lives" within
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import ErrorBoundry from '../components/ErrorBoundry';
 import Scroll from '../components/Scroll';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchField: "",
-    }
-  }
+function App() {
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     robots: [],
+  //     searchField: "",
+  //   }
+  // }
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState("");
 
-  //method used to connnect and get info from api
-  componentDidMount() {
+  //hook used to connnect and get info from api
+  //second argument (optional) => if the contained 
+  //  items are changed then useEffect is ran again, 
+  //  if [] is left blank acts similar to 
+  //  componentDidMount(); 
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => res.json())
-      .then(users => this.setState({ robots: users }));
-  }
+    .then(res => res.json())
+      .then(users => setRobots(users));
+  }, []);
 
   //method to pass to child component to get feedback 
   //and change state from end user
-  onSearchChange = (event) => {
-    this.setState({ searchField: event.target.value });
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value);
   }
 
   //necessary method in React
-  render() {
-    //deconstruct state for cleaner use
-    const { robots, searchField } = this.state;
-    //filters array to find end user search
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase())
-    });
+  // render() {
+  //filters array to find end user search
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase())
+  });
 
-    //return necessary in React
-    //condition statement to make sure robot array 
-    //  getting data from api
-    return (!robots.length) ?
-      <h1>Loading</h1> :
+  //return necessary in React
+  //condition statement to make sure robot array 
+  //  getting data from api
+  return (!robots.length) ?
+    <h1>Loading</h1> :
+    (
       <div className="tc">
         <h1 className='f1'>Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         {/* Scroll Component for better UX*/}
         <Scroll>
           {/* ErrorBoundry is used to check for loading issues*/}
@@ -53,7 +58,7 @@ class App extends Component {
           </ErrorBoundry>
         </Scroll>
       </div>
-  }
+    )
 }
 
 export default App;
